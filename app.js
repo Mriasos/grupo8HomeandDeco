9a1687c51dc9eb0b2923b1129fea485e2aa91f99
thiagomenin5 <tatimenin50@gmail.com>
2022-10-03 21:28:54 -0300

    agregando carpetas y archivos

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const publicPath = path.resolve('public');
+app.use(express.static(publicPath));
+
+const puerto = 3000;
+app.listen(puerto, ()=> {
+    console.log('iniciando')
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve('home/index.html'))
+})
