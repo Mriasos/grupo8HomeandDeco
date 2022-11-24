89937c828cd9ec6673a5efe925eee8e3cc40c89a
Mriasos <maridalso@hotmail.com>
2022-11-24 08:07:53 -0300

    se completo instalacion multer con diskstorage,destination filnme y upload en authrouter. En register ejs modificacion de action loggin.html x login e incorporaciòn method.

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,10 +1,28 @@
 let express = require('express')
 let router = express.Router()
+const multer = require('multer');
+const { dirname } = require('path');
+const path = require('path');
 const authController = require ('../../src/controllers/authController')
 
-const multer = require('multer')
+//se indica a Multer donde se guardaran los archivos y con que nombre
+const storage = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, path.join(--dirname, '../public/imagenes/fotoperfil' ))
+    },
+    //nombre que se le dara al archivo 
+    filename: (req, file, cb) =>{
+        const newFilename = 'imagperfil-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFilename );
+    }
+});
+//ejecucion  
+const upload = multer ({storage});
 
 router.get('/register', authController.renderRegister)
+
+//Procesamiento de formulario
+//router.post('/', upload.single('imagenUsuario'), authController.store)
 
 router.get('/login', authController.renderLoggin)
 
