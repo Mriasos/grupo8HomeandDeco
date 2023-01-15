489a4eac414b1aab7b2a5ce2fbf422658144d67a
thiagomenin5 <tatimenin50@gmail.com>
2023-01-15 19:27:14 -0300

    models de mediosDePago terminado

---
+++ src/database/models/mediosdepago.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Mediosdepago extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Mediosdepago.init({
+    id_mediosDePago: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    tarjetaDeCredito: {
+      type: DataTypes.INTEGER.TINYINT,
+      allowNull:false
+    },
+    transferencia: {
+      type: DataTypes.INTEGER.TINYINT,
+      allowNull:false
+    },
+    billetera: {
+      type: DataTypes.INTEGER.TINYINT,
+      allowNull:false
+    },
+    mercadoLibre: {
+      type: DataTypes.INTEGER.TINYINT,
+      allowNull:false
+    },
+    id_ventas: {
+      type: DataTypes.INTEGER,
+      allowNull:false
+    },
+  }, {
+    sequelize,
+    modelName: 'Mediosdepago',
+    tableName: 'mediosdepago'
+  });
+  return Mediosdepago;
+};
